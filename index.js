e31e4bfaccb790a0f96783c2ef8c49d637cd73c0
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-05 21:38:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  return `${string.toLowerCase()} 
+  return `${string.toLowerCase()}
   if (string = string.toLowerCase())
 }
