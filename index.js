e2484332485216959cc1092603c11205877014cb
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-05 21:53:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (string = string.toLowerCase()){
   return ("I can't hear you!")}
-  if (string = string.toUpperCase()){
-      console.log ("YES INDEED")
-    }
 }
+function sayHiToGrandma(string){
+  if (string = string.toUpperCase()){
+      return ("YES INDEED")
+    }
